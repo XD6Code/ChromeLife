87618447ebd4be46b9079cf0b4e7ff9c1a19b283
Charles Hollenbeck <charles@hollenbeck.pw>
2013-09-30 00:54:55 -0400

    Update version

--- versions.js
+++ versions.js
@@ -8,6 +8,10 @@ var distant = ["Only show desktop notifications for certain friends",
                "SL Marketplace support"];
 
 var versions = [
+    {version: "1.0.9.2",
+	    name: "Scrolling to solutions",
+	 changes: ["Removed scrollbar from popout", "Added custom scrollbar to options page"],
+	   fixes: ["Fixed positioning of icon badge on the icon preview"]},
     {version: "1.0.9",
 	    name: "Strapping on a new Interface",
 	 changes: ["Complete overhual of the options page's user interface"],
